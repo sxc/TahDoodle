3468f6a587077ca30b2e9f7b69bf2c7a54333498
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-01-05 16:39:13 +0800

    Initial Commit

---
+++ TahDoodle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TahDoodle
+//
+//  Created by Koharu on 2023/1/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TahDoodle/TahDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahDoodleApp.swift
+//  TahDoodle
+//
+//  Created by Koharu on 2023/1/5.
+//
+
+import SwiftUI
+
+@main
+struct TahDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
